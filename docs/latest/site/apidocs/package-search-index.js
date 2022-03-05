f87c2979fbe56abd58f0de810e6c1a7474aab1f3
oak <oak@users.noreply.github.com>
2022-03-05 14:21:12 +0000

    Deploying to gh-pages from @ oak/sbs4j@c2a1e113e5ce8ca99dd817119d45547f1230a462 🚀

---
+++ docs/latest/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dev.oak3.sbs4j"},{"l":"dev.oak3.sbs4j.exception"},{"l":"dev.oak3.sbs4j.helper"},{"l":"dev.oak3.sbs4j.interfaces"}]
